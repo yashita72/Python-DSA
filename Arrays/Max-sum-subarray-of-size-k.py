c03776c8dce80ec580a7c3c7f7820e88b0925660
Yashita Gaur <yashitagaur72@gmail.com>
2026-07-03 21:19:39 +0530

    solve max sum subarray of size k

--- Arrays/Max-sum-subarray-of-size-k.py
+++ Arrays/Max-sum-subarray-of-size-k.py
@@ -1,4 +1,20 @@
 class Solution:
-    def Maxsum(arr,k):
+    def maxSum(self, arr, k):
+        n = len(arr)
         
+        # sum of first window
+        window_sum = 0
+        for i in range(k):
+            window_sum += arr[i]
+        
+        max_sum = window_sum
+        
+        # slide the window
+        for j in range(k, n):
+            window_sum = window_sum - arr[j - k] + arr[j]
+            if window_sum > max_sum:
+                max_sum = window_sum
+        
+        return max_sum
+    #time complexity big oh of n
     
