3b1ea64ff3c7f17fea4e9c4752f220a8ea55d425
Yashita Gaur <yashitagaur72@gmail.com>
2026-07-13 23:27:52 +0530

    commit

--- Arrays/rotated-array-left-rotation.py
+++ Arrays/rotated-array-left-rotation.py
@@ -10,14 +10,12 @@ class Solution:
            right -=1
         left = k
         right = len(nums)-1
-
         while left < right:
            nums[left], nums[right] = nums[right], nums[left]
            left += 1
            right -=1
         left = 0
         right = len(nums) - 1
-       
         while left < right:
            nums[left], nums[right] = nums[right], nums[left]
            left += 1
