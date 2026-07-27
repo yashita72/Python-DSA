f4cc9532844d921df242dff15c519a087f762a75
Yashita Gaur <yashitagaur72@gmail.com>
2026-07-27 18:02:20 +0530

    solved stack

---
+++ Linkedlist/Add-two-numbers.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 is not None or l2 is not None:
+            val1 = l1.val if l1 is not None else 0
+            val2 = l2.val if l2 is not None else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+
+        if carry > 0:
+            current.next = ListNode(carry)
+
+        return dummy.next
