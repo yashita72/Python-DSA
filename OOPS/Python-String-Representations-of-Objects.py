c4003b35ccd7a63632c709f14d981ee37da31fbc
Yashita Gaur <yashitagaur72@gmail.com>
2026-07-28 13:34:00 +0530

    Python: String Representations of Objects solved on hackerrank

---
+++ OOPS/Python-String-Representations-of-Objects.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, maximum_speed, unit):
+        self.maximum_speed = maximum_speed
+        self.unit = unit
+
+    def __str__(self):
+        return f"Car with the maximum speed of {self.maximum_speed} {self.unit}"
+
+
+class Boat:
+    def __init__(self, maximum_speed):
+        self.maximum_speed = maximum_speed
+
+    def __str__(self):
+        return f"Boat with the maximum speed of {self.maximum_speed} knots"
