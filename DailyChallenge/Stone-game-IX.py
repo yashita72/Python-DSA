d9500bd6ff5f1f2264e346ce71971a42de7ca44c
Yashita Gaur <yashitagaur72@gmail.com>
2026-09-02 21:13:27 +0530

    DailyChallenge/Stone-game-IX.py

---
+++ DailyChallenge/Stone-game-IX.py
@@ -0,0 +1,9 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt = [0, 0, 0]
+        for s in stones:
+            cnt[s % 3] += 1
+        
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
+        return abs(cnt[1] - cnt[2]) > 2
